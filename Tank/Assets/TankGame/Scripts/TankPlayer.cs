27b425e8d4098f10703cd6b94bf56d98f27075d9
JonesGod <45507991+JonesGod@users.noreply.github.com>
2021-12-14 21:03:21 +0800

    FirstPush1214
    FirstPush

---
+++ Tank/Assets/TankGame/Scripts/TankPlayer.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+namespace Tank
+{
+    public class TankPlayer : MonoBehaviourPunCallbacks
+    {
+        //Reference to tank's movement script, used to disable and enable control.
+        private Complete.TankMovement m_Movement;
+        // Reference to tank's shooting script, used to disable and enable control.
+        private Complete.TankShooting m_Shooting;
+
+        void Awake()
+        {
+            m_Movement = GetComponent<Complete.TankMovement>();
+            m_Shooting = GetComponent<Complete.TankShooting>();
+
+            if(!photonView.IsMine)
+            {
+                m_Movement.enabled = false;
+                m_Shooting.enabled = false;
+
+                enabled = false;
+            }
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
+
+
